350f7c956d06fed37ff62ada85e1a7780e3a328d
Nikki612 <86887514+Nikki612@users.noreply.github.com>
2022-12-27 16:50:07 +0200

    commit from the smol one

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,14 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { sequelize } from "./sequelize.js";
-import { MovieRouter } from "./roots/moviesRouter.js";
+import { UserRouter } from "./routers/userRouter.js";
 
 const port=5001;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/api",MovieRouter)
+app.use("/api",UserRouter)
 app.listen(port,async ()=>{
     try {
         sequelize.authenticate();
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,53 +1,18 @@
-import { User } from "../models/user.js";
-import {Op} from "sequelize"
+import express from "express";
+import * as UserController from "../controllers/userController.js";
 
-//INSERT INTO METHOD
+const router=express.Router();
 
-const insertUserIntoDatabase=async (req,res)=>{
+router.post("/newUser", UserController.insertUserIntoDatabase);
+router.get("/users", UserController.getAllUsersFromDB); // select *
+router.get("/users/:userId", UserController.getUserFromDBById); //select from where pk=moveiId
 
-    try{
-        const receivedUser=req.body;
-        await User.create(receivedUser);
-        res.status(200).json("Object has been added to DB!"); //for success 
-    }
-    catch(err){
-        return res.status(500).json(err) //for client error
-    }
-}
+export {router as UserRouter};
 
-const getAllUsersFromDB=async (req,res)=>{
 
-    try{
-        const users=await User.findAll();
-        return res.status(200).json(users);
 
-    }
-    catch(err)
-    {
-        res.status(500).json(err) 
-    }
-}
 
-const getUserFromDBById=async (req,res)=>{
-    try{
-        const user=await User.findByPk(req.params.id);
-        if(user)
-        {
-            return res.status(200).json(user);
-        }
-        else
-        {
-            return res.status(404).json({error:"Not Found"});
-        }
-    }
-    catch(err){
-        res.status(500).json(err);
-    }
-};
 
-export
-{
-    insertUserIntoDatabase,
-    getAllUsersFromDB,
-    getUserFromDBById
-};
+
+
+
