028196ccfa61cc2440dc949c897685cb87a97d55
lipanr007 <rrsed123@gmail.com>
2023-01-14 17:30:43 +0200

    changed storage

--- backend/sequelize.js
+++ backend/sequelize.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const sequelize=new Sequelize({
     dialect:"sqlite",
-    storage:"./database/bugapp.db"
+    storage:"./backend/database/bugapp.db"
 })
 sequelize.sync({alter:true}).then(()=>{    //alter true checks if the table has been created,if yes we just update
     console.log("All the models have been synced")
