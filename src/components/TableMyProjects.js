c6060c6f40183c8afda6344b2776d273a4ad9ba3
Nikki612 <86887514+Nikki612@users.noreply.github.com>
2023-01-14 16:27:58 +0200

    teable_my_proj

---
+++ src/components/TableMyProjects.js
@@ -0,0 +1,149 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+function createData(id, name, description, repo) {
+  return {
+    id,
+    name,
+    description,
+    repo,
+    history: [
+      {
+        id: 1,
+        severity: "ooglie booglie",
+        description: 3,
+        link:'https://'
+      },
+      {
+        id: 2,
+        severity: "ooglie booglie",
+        description: 3,
+        link:'https://'
+      },
+    ],
+  };
+}
+
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell align="right">{row.calories}</TableCell>
+        <TableCell align="right">{row.fat}</TableCell>
+        <TableCell align="right">{row.carbs}</TableCell>
+        <TableCell align="right">{row.protein}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                History
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Severity</TableCell>
+                    <TableCell align="right">Description</TableCell>
+                    <TableCell align="right">Link</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.history.map((historyRow) => (
+                    <TableRow key={historyRow.id}>
+                      <TableCell component="th" scope="row">
+                        {historyRow.id}
+                      </TableCell>
+                      <TableCell>{historyRow.severity}</TableCell>
+                      <TableCell align="right">{historyRow.description}</TableCell>
+                      <TableCell align="right">
+                        {historyRow.link}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+Row.propTypes = {
+  row: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    repo: PropTypes.string.isRequired,
+    history: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        severity: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        link:PropTypes.string.isRequired
+      }),
+    ).isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+const rows = [
+  createData(1,'alibaba', 'ching chong', 'http://link la baieti'),
+  createData(2,'alibaba', 'ching chong', 'http://link la baieti'),
+  createData(3,'alibaba', 'ching chong', 'http://link la baieti'),
+  createData(4,'alibaba', 'ching chong', 'http://link la baieti'),
+  createData(5,'alibaba', 'ching chong', 'http://link la baieti'),
+];
+
+export default function CollapsibleTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell> No</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Description</TableCell>
+            <TableCell align="right">Repo</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <Row key={row.id} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
