0502af3f504df0a2911af5581d4563c2b5821c31
Nikki612 <86887514+Nikki612@users.noreply.github.com>
2023-01-15 15:36:43 +0200

    WORKING AUTOCOMPLETE HALELUJAH

--- frontend/src/screens/AddProjectScreen.jsx
+++ frontend/src/screens/AddProjectScreen.jsx
@@ -4,9 +4,17 @@ import Button from '@mui/joy/Button';
 import Box from '@mui/joy/Box';
 import Typography from '@mui/joy/Typography';
 import Input from '@mui/joy/Input';
-import { Autocomplete } from '@mui/material';
+import { Autocomplete,createFilterOptions} from '@mui/material';
 import {TextField} from '@mui/material';
 import axios from 'axios';
+import parse from 'autosuggest-highlight/parse';
+import match from 'autosuggest-highlight/match';
+import CircularProgress from '@mui/material/CircularProgress';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogActions from '@mui/material/DialogActions';
 
 const style = {
     position: 'absolute',
@@ -21,23 +29,84 @@ const style = {
   };
 
 function AddProjectsScreen() {
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  const [users,setUsers]=useState('')
+ 
+  const filter = createFilterOptions();
 
-  useEffect(() => {
-    function completeList()
-    {
-      axios
-      .get('http://localhost:5001/api/users')
-      .then((response) => {
-        setUsers(response.data);
-        console.log("Response",response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    }})
+  const [open, setOpen] = React.useState(false);
+  const [options, setOptions] = React.useState([]);
+  const loading = open && options.length === 0;
+
+
+  const [users, setCategories] = useState([]);
+
+  const [openDialog, toggleOpenDialog] = React.useState(false);
+
+  
+  const handleClose = () => {
+    setDialogValue({
+      email: '',
+      slug: '',
+      image: '',
+    });
+
+    toggleOpenDialog(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCategories({
+      email: dialogValue.email
+    });
+
+    handleClose();
+  };
+
+
+  const [dialogValue, setDialogValue] = React.useState({
+    email: ''
+
+  });
+
+
+  React.useEffect(() => {
+      let active = true;
+
+      if (!loading) {
+          return undefined;
+      }
+
+      (async () => {
+
+          var config = {
+              method: 'get',
+              url: 'http://localhost:5001/api/users',
+          };
+
+          axios(config)
+              .then(function (response) {
+                  response.data = response.data.filter((category) => category.email)
+                  if (active) {
+                      setOptions([...response.data]);
+                  }
+
+              })
+              .catch(function (error) {
+                  console.log(error);
+          });
+
+
+      })();
+
+      return () => {
+          active = false;
+      };
+  }, [loading]);
+
+  React.useEffect(() => {
+      if (!open) {
+          setOptions([]);
+      }
+  }, [open]);
     
   return (
     <div>
@@ -72,21 +141,111 @@ function AddProjectsScreen() {
                 sx={{ mb: 1, fontSize: 'var(--joy-fontSize-sm)' }}
               />
               <Autocomplete
+                id="async-list"
+                open={open}
+                limitTags={2}
+                onOpen={() => {
+                    setOpen(true);
+                }}
+                onClose={() => {
+                    setOpen(false);
+                }}
+                isOptionEqualToValue={(option, value) => option.email === value.email}
+                getOptionLabel={(option) => {
+                    // Value selected with enter, right from the input
+                    if (typeof option === 'string') {
+                        return option;
+                    }
+                    // Add "xxx" option created dynamically
+                    if (option.inputValue) {
+                        return option.inputValue;
+                    }
+                    // Regular option
+                    return option.email;
+                }}
+                options={options}
+                loading={loading}
                 multiple
-                id="tags-standard"
-                options={users}
-                getOptionLabel={(users) => users.email}
                 renderInput={(params) => (
-                <TextField
-                    {...params}
-                    variant="standard"
-                    label="Team Members"
-                    placeholder="Favorites"
-                />
+                    <>
+                        <TextField
+                            {...params}
+                            label="Team members"
+                            InputProps={{
+                                ...params.InputProps,
+                                endAdornment: (
+                                    <React.Fragment>
+                                        {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                                        {params.InputProps.endAdornment}
+                                    </React.Fragment>
+                                ),
+                            }}
+                        />
+                        {console.log(options)}
+                    </>
                 )}
-      />
+                renderOption={(props, option, { inputValue }) => {
+                    const matches = match(option.email, inputValue);
+                    const parts = parse(option.email, matches);
+      
+                    return (
+                        <li {...props}>
+                            <div>
+                                {parts.map((part, index) => (
+                                    <span
+                                        key={index}
+                                        style={{
+                                            color: part.highlight ? "red" : 'inherit',
+                                            fontWeight: part.highlight ? 700 : 400,
+                                        }}
+                                    >
+                                      {part.text}
+                                    </span>
+                                ))}
+                            </div>
+                        </li>
+                    );
+                }}
+      
+                value={users}
+                onChange={(event, newValue) => {
+                  if (typeof newValue === 'string') {
+                    // timeout to avoid instant validation of the dialog's form.
+                    setTimeout(() => {
+                      toggleOpenDialog(true);
+                      setDialogValue({
+                        email: newValue
+                       });
+                    });
+                  } else if (newValue && newValue.inputValue) {
+                    toggleOpenDialog(true);
+                    setDialogValue({
+                      email: newValue.inputValue,
+                    });
+                  } else {
+                    setCategories(newValue);
+                  }
+        
+                }}
+                filterOptions={(options, params) => {
+                    const filtered = filter(options, params);
+                    const { inputValue } = params;
+                    const isExisting = options.some((option) => inputValue === option.email);
+                    if (inputValue !== '' && !isExisting) {
+                        filtered.push({
+                            inputValue:inputValue,
+                            email: `Add "${inputValue}"`,
+                        });
+                    }
+                    return filtered;
+                }}
+                selectOnFocus
+                clearOnBlur
+                handleHomeEndKeys
+      
+            />
             </form>
-            <Button onClick={handleClose} type="submit">
+            <Button type="submit">
               Submit
             </Button>
           </Box>
