076b9517361629cc8a8e04ce2912f3e35887b5dd
meranalexandru <meraneugen20@stud.ase.ro>
2023-01-16 21:42:21 +0200

    hai ca se poate

--- backend/app.js
+++ backend/app.js
@@ -1,43 +1,41 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import { sequelize } from "./sequelize.js";
-import { UserRouter } from "./routers/userRouter.js";
-import { User } from "./models/user.js";
-import { Bug } from "./models/bug.js";
-import { Project } from "./models/project.js";
-import { ProjectMember } from "./models/projectmember.js";
-import { BugRouter } from "./routers/bugRouter.js";
-import { ProjectRouter } from "./routers/projectRouter.js";
-import { ProjectMemberRouter } from "./routers/projectMemberRouter.js";
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import { sequelize } from './sequelize.js'
+import { UserRouter } from './routers/userRouter.js'
+import { User } from './models/user.js'
+import { Bug } from './models/bug.js'
+import { Project } from './models/project.js'
+import { ProjectMember } from './models/projectmember.js'
+import { BugRouter } from './routers/bugRouter.js'
+import { ProjectRouter } from './routers/projectRouter.js'
+import { ProjectMemberRouter } from './routers/projectMemberRouter.js'
 
-const port = 5001;
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/api", UserRouter);
-app.use("/api", BugRouter);
-app.use("/api", ProjectRouter);
-app.use("/api", ProjectMemberRouter);
+const port = 5001
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/api', UserRouter)
+app.use('/api', BugRouter)
+app.use('/api', ProjectRouter)
+app.use('/api', ProjectMemberRouter)
 
 app.listen(port, async () => {
   try {
-    sequelize.authenticate();
-    console.log("Authentication successful!");
+    sequelize.authenticate()
+    console.log('Authentication successful!')
   } catch (error) {
-    console.error("Unable to connect to the database", error);
+    console.error('Unable to connect to the database', error)
   }
-});
-console.log("API is running at port: ", port);
+})
+console.log('API is running at port: ', port)
 
 //TODO: TABLE RELATIONSHIPS
 
-User.belongsToMany(Project, {though: ProjectMember});
-Project.belongsToMany(User, {though: ProjectMember});
+User.belongsToMany(Project, { through: ProjectMember })
+Project.belongsToMany(User, { through: ProjectMember })
 Bug.belongsTo(Project)
 Project.hasMany(Bug)
 
-await sequelize.sync({force:true});
-
-
+await sequelize.sync({ force: true })
